5a4719fd905266737a08a2d1b373f18b56b1f076
José Fernando de Oliveira Filho <57115855+fernandooliveira7@users.noreply.github.com>
2024-09-14 18:31:24 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/rendimento_page.dart';
-import 'pages/gastos_page.dart'; // Importando a nova página
+import 'pages/gastos_page.dart'; // Importando a página de gastos
+import 'pages/listagastos_page.dart'; // Importando a página de lista de gastos
 
 void main() {
   runApp(MyApp());
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/rendimento': (context) => RendimentoPage(), // Rota para a página de rendimento
         '/gastos': (context) => GastosPage(),         // Rota para a página de gastos
+        '/listagastos': (context) => ListaDeGastosPage(), // Rota para a página de lista de gastos
       },
     );
   }
